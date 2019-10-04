08ba9826858584c6800dac27b711f28a739d7d4e
Rody Davis <rody.davis.jr@gmail.com>
2019-10-04 14:51:34 -0400

    Update md_render.dart

--- lib/ui/common/md_render.dart
+++ lib/ui/common/md_render.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_html/flutter_html.dart';
-import 'package:html/dom.dart' as dom;
 import 'package:markdown/markdown.dart' as md;
-import 'package:personal_website/ui/demos/index.dart';
 
-import '../../data/blocs/blocs.dart';
 import '../../plugins/url_launcher/url_launcher.dart';
-import '../router.dart';
+import '../demos/index.dart';
 
 class MarkdownRender extends StatelessWidget {
   const MarkdownRender({
@@ -17,8 +13,6 @@ class MarkdownRender extends StatelessWidget {
         super(key: key);
 
   final String _html;
-
-  get UriUtils => null;
 
   @override
   Widget build(BuildContext context) {
@@ -32,11 +26,11 @@ class MarkdownRender extends StatelessWidget {
                   child: Container(
                     width: 900,
                     child: Html(
-                      data: md.markdownToHtml(_html),
-                      //Optional parameters:
-                      // routes: Router.routes(blocState),
+                      data: md.markdownToHtml(
+                        _html,
+                        extensionSet: md.ExtensionSet.gitHubWeb,
+                      ),
                       routes: {
-                        // -- Demos --
                         TestDemo.routeName: (_) => TestDemo(),
                         PianoFinalDemo.routeName: (_) => PianoFinalDemo(),
                       },
@@ -47,26 +41,7 @@ class MarkdownRender extends StatelessWidget {
                       linkStyle: const TextStyle(color: Colors.redAccent),
                       onLinkTap: (url) => UrlUtils.open(url, name: 'Info'),
                       onImageTap: (src) => UrlUtils.open(src, name: 'Preview'),
-                      //Must have useRichText set to false for this to work.
                       useRichText: true,
-                      // customTextAlign: (dom.Node node) {
-                      //   if (node is dom.Element) {
-                      //     switch (node.localName) {
-                      //       case "p":
-                      //         return TextAlign.justify;
-                      //     }
-                      //   }
-                      // },
-                      // customTextStyle: (dom.Node node, TextStyle baseStyle) {
-                      //   if (node is dom.Element) {
-                      //     switch (node.localName) {
-                      //       case "p":
-                      //         return baseStyle
-                      //             .merge(TextStyle(height: 2, fontSize: 20));
-                      //     }
-                      //   }
-                      //   return baseStyle;
-                      // },
                     ),
                   ),
                 ),
